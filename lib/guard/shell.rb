1fd8b6da3e9748061f7bc73a387ca9de272b7bd6
Ian Yang <me@iany.me>
2012-04-01 06:46:43 +0800

    add option run_all to execute an arbitrary proc

--- lib/guard/shell.rb
+++ lib/guard/shell.rb
@@ -6,6 +6,14 @@ module Guard
   
     VERSION = '0.1.1'
 
+    def start
+      run_all
+    end
+
+    def run_all
+      run_on_change([options[:run_all].call]) if options[:run_all]
+    end
+
     # Print the result of the command, if there is a result
     # to be printed. (see README.md)
     #
